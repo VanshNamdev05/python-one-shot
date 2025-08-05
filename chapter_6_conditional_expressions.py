7d3db2e8f410b4d57769dbd895a51a05a66f0eb8
VanshNamdev05 <realvn05@gmail.com>
2025-08-05 19:47:18 +0530

    chapter 7 conditional expressions done

---
+++ chapter_6_conditional_expressions.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age : "))
+
+if(a>18):
+    print("You are 18+")
+    
+elif(a<0):
+    print("Your have entered invalid age")
+
+else:
+    print("You are child")
+
+print("End of conditional program")
---
+++ chapter_6_practice_set.py
@@ -0,0 +1,30 @@
+# a1 = int(input("Enter the number 1 : "))
+# a2 = int(input("Enter the number 2 : "))
+# a3 = int(input("Enter the number 3 : "))
+# a4 = int(input("Enter the number 4 : "))
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print(a1,"a1 is biggest")
+
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print(a3,"a2 is biggest")
+
+# elif(a3>a2 and a3>a1 and a3>a4):
+#     print(a2,"a3 is biggest")
+
+# else:
+#     print(a4, "a4 is biggest")
+    
+# username = input("Enter your name : ")
+
+# if(len(username)<10):
+#     print("Your name is short")
+# else:
+#     print("Your name is perfect")
+    
+post = input("Enter the post : ")
+
+if("VaNsH".lower() in post.lower()):
+    print("This post is talking about vansh")
+else:
+    print("This post is not talking about vansh")
